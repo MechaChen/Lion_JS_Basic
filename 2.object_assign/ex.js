719397e0d27f969c398cdd239ac58310acc983c9
Benson <tumlivein@gmail.com>
2019-08-23 15:27:57 +0800

    Array assign 問題

--- 2.object_assign/ex.js
+++ 2.object_assign/ex.js
@@ -1,3 +1,4 @@
+// Object
 var o1 = {};
 var o2 = o1;
 
@@ -6,3 +7,13 @@ console.log(o2.a); // 1
 
 o2.a = 2;
 console.log(o1.a); // 2
+
+// Array
+var a1 = [];
+var a2 = a1;
+
+a1[0] = 1;
+console.log(a2[0]); // 1
+
+a2[0] = 2;
+console.log(a1[0]); // 2
